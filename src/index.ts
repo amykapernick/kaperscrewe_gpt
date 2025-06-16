e323dc7e7ecc909ce471fc6438cac13c57789512
Amy Kapernick <amy@kapers.dev>
2025-06-16 09:10:10 +0000

    Trying again

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-import './functions/generateResponse';
+import { app } from '@azure/functions';
 import './functions/fetchTasks';
+import './functions/generateResponse';
+import './functions/httpTrigger1';
+
+app.setup({
+	enableHttpStream: true,
+});
